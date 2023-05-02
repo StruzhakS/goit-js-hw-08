012c4a453c73d8ac0e6109d08be3ca65f8b2060a
StruzhakS <StruzhakS@gmail.com>
2023-05-02 08:53:54 +0300

    task-2 do)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(reloadPlay, 1000));
+
+function reloadPlay(ev) {
+  const currentSeconds = ev.seconds;
+  console.log(currentSeconds);
+  localStorage.setItem('videoplayer-current-time', currentSeconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
