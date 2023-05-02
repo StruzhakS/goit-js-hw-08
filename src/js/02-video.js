adb88035a8dd51433e222cb34ff2a9ce66fed994
StruzhakS <StruzhakS@gmail.com>
2023-05-02 15:36:29 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,6 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 
 player.on('timeupdate', throttle(reloadPlay, 1000));
-// player.on('play', throttle(reloadPlay, 1000));
 
 function reloadPlay(ev) {
   currentSeconds = ev.seconds;
@@ -12,9 +11,4 @@ function reloadPlay(ev) {
   console.log(currentSeconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-// player.off('play', function (ev) {
-//   //   ev.seconds = localStorage.getItem('videoplayer-current-time');
-//   console.log(ev);
-// });
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
